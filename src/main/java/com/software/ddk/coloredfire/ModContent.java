a5de77a272fb2e1ea53493be69db5e2baecf2d9d
Diegoddk <diegosxtremo64@gmail.com>
2019-11-22 15:32:42 -0300

    added green fire. added green flint and steel added recipes.

--- src/main/java/com/software/ddk/coloredfire/ModContent.java
+++ src/main/java/com/software/ddk/coloredfire/ModContent.java
@@ -1,8 +1,10 @@
 package com.software.ddk.coloredfire;
 
 import com.software.ddk.coloredfire.common.block.BlueFireBlock;
+import com.software.ddk.coloredfire.common.block.GreenFireBlock;
 import com.software.ddk.coloredfire.common.block.RedFireBlock;
 import com.software.ddk.coloredfire.common.item.FlintAndSteelBlueItem;
+import com.software.ddk.coloredfire.common.item.FlintAndSteelGreenItem;
 import com.software.ddk.coloredfire.common.item.FlintAndSteelRedItem;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
@@ -11,17 +13,22 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModContent {
-
     public static Block RED_FIRE_BLOCK = new RedFireBlock();
     public static Block BLUE_FIRE_BLOCK = new BlueFireBlock();
+    public static Block GREEN_FIRE_BLOCK = new GreenFireBlock();
+
     public static Item FLINT_AND_STEEL_RED = new FlintAndSteelRedItem(new Item.Settings().group(ItemGroup.TOOLS));
     public static Item FLINT_AND_STEEL_BLUE = new FlintAndSteelBlueItem(new Item.Settings().group(ItemGroup.TOOLS));
+    public static Item FLINT_AND_STEEL_GREEN = new FlintAndSteelGreenItem(new Item.Settings().group(ItemGroup.TOOLS));
 
     public static void RegisterAll(){
         Registry.register(Registry.BLOCK, new Identifier(ColoredFireMod.MODID, "red_fire"), RED_FIRE_BLOCK);
         Registry.register(Registry.BLOCK, new Identifier(ColoredFireMod.MODID, "blue_fire"), BLUE_FIRE_BLOCK);
+        Registry.register(Registry.BLOCK, new Identifier(ColoredFireMod.MODID, "green_fire"), GREEN_FIRE_BLOCK);
+
         Registry.register(Registry.ITEM, new Identifier(ColoredFireMod.MODID, "flint_and_steel_red"), FLINT_AND_STEEL_RED);
         Registry.register(Registry.ITEM, new Identifier(ColoredFireMod.MODID, "flint_and_steel_blue"), FLINT_AND_STEEL_BLUE);
+        Registry.register(Registry.ITEM, new Identifier(ColoredFireMod.MODID, "flint_and_steel_green"), FLINT_AND_STEEL_GREEN);
 
     }
 }
